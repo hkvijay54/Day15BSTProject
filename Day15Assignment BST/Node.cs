03c94a77c462626d29e43f39f375333aacc7d65c
hkvijay54 <hkvijay54@gmail.com>
2022-01-10 01:32:02 +0530

    uc1

---
+++ Day15Assignment BST/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15Assignment_BST
+{
+    class Node
+    {
+        public int data;
+        public Node LeftNode;
+        public Node RightNode;
+
+        public Node()
+        {
+            this.LeftNode = null;
+            this.RightNode = null;
+        }
+    }
+}
