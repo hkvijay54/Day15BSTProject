42bcb5537932b862078bca9e215ef7ac47017f6f
hkvijay54 <hkvijay54@gmail.com>
2022-01-10 01:59:27 +0530

    uc3

--- Day15Assignment BST/Program.cs
+++ Day15Assignment BST/Program.cs
@@ -5,25 +5,19 @@ namespace day15_binary_tree
 {
     class Program
     {
-        //Ability to create the binary tree shown in thefigure -Check if all are added with using size method in Binary Tree
         static void Main(string[] args)
         {
-            BinaryTree obj = new BinaryTree();
-            obj.Insert(56);
-            obj.Insert(30);
-            obj.Insert(70);
-            obj.Insert(22);
-            obj.Insert(40);
-            obj.Insert(60);
-            obj.Insert(95);
-            obj.Insert(11);
-            obj.Insert(65);
-            obj.Insert(3);
-            obj.Insert(16);
-            obj.Insert(63);
-            obj.Insert(67);
-            obj.TraversePreorder(obj.root);
+
+            BinaryTree obj1 = new BinaryTree();
+            obj1.Insert(56);
+            obj1.Insert(30);
+            obj1.Insert(70);
+            obj1.Insert(63);
+            obj1.TraversePreorder(obj1.root);
             Console.WriteLine();
+            bool isFound = obj1.SearchBST(obj1.root, 63);
+            Console.WriteLine("63 is found " + isFound);
+
         }
     }
 }
